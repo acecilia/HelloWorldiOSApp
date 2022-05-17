5f9f5382342350080613558acd9757518fd4d48c
Andres Cecilia Luque <andres.luque@revolut.com>
2022-05-17 18:57:15 +0100

    Initial commit

---
+++ SingleViewApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SingleViewApp
+//
+//  Created by Andrés Cecilia Luque on 17/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+    }
+}
+
